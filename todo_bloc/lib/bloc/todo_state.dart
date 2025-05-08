0cc428a834b712b6dff937332a713c0588ca62af
IrfanHidayatulahP <irfanhputra400@gmail.com>
2025-05-08 17:43:53 +0700

    membuat class TodoBlocInitial, TodoLoading, dan TodoLoaded

--- todo_bloc/lib/bloc/todo_state.dart
+++ todo_bloc/lib/bloc/todo_state.dart
@@ -1,6 +1,19 @@
 part of 'todo_bloc.dart';
 
-@immutable
 sealed class TodoState {}
 
 final class TodoInitial extends TodoState {}
+
+final class TodoBlocInitial extends TodoState {}
+
+final class TodoLoading extends TodoState {}
+
+final class TodoLoaded extends TodoState {
+  final List<Todo> todos;
+  DateTime? selectedDate;
+
+  TodoLoaded({
+    required this.todos,
+    required this.selectedDate,
+  });
+}
